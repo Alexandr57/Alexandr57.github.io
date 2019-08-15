80f3ca3e3bce70e64721f192f293241fe90e3708
Александр <borisov.alexandr.7@gmail.com>
2019-08-15 22:27:44 +0500

    Update Project "Snow Princess" Demo version 2

--- Projects/Snow-Princess/js/plugins/AScripts.js
+++ Projects/Snow-Princess/js/plugins/AScripts.js
@@ -3,28 +3,35 @@
 //=============================================================================
 
 /*:ru
- * @plugindesc AScripts Версия REL_1_Fix_1 Дополнительные скрипты
+ * @plugindesc AScripts Версия v1.1-b Дополнительные скрипты
  * @author Alexandr_7
  * @help
-Здравствуйте. Данный плагин будет содержать дополнительные скрипты для облегчения работы. Плагин распространяется бесплатно для комерческого и некомерческого использования.
+ Здравствуйте. Данный плагин будет содержать дополнительные скрипты для облегчения работы. Плагин распространяется бесплатно для комерческого и некомерческого использования.
 
-Сразу скажу что запрещено чужие скрипты выдавать за свои. И еще просьба тем кому не лень добавлять сюда свои скрипты.Можно добавлять любые скрипты которые могут облечить работу с JavaScript например уменьшить очень длинные скрипты.
+ Сразу скажу что запрещено чужие скрипты выдавать за свои. И еще просьба тем кому не лень добавлять сюда свои скрипты. Можно добавлять любые скрипты которые могут облечить работу с JavaScript например уменьшить очень длинные скрипты.
 
-Вызов скриптов:
-AScripts.СКРИПТ
-Где СКРИПТ 	это скрипт который хотим вызвать.
+ Вызов скриптов:
+ AScripts.СКРИПТ
+ Где СКРИПТ 	это скрипт который хотим вызвать.
 
-Скрипты добавленные Alexandr_7:
-getRandomInRange(min, max) - Пример AScripts.getRandomInRange(10, 20) (Вернет от 10 до 20 включительно) Получения рандомного числа от и до включая минимальное и максимальное.
-coinToss() - Пример AScripts.coinToss() (Вернет 0 или 1) Получения рандомного числа 0 или 1
-coinTossBool() - Пример AScripts.coinTossBool() (Вернет true или false) Получения рандомно true или false
+ Скрипты добавленные Alexandr_7:
+ getRandomInRange(min, max) - Пример AScripts.getRandomInRange(10, 20) (Вернет от 10 до 20 включительно) Получения рандомного числа от и до включая минимальное и максимальное.
+ coinToss() - Пример AScripts.coinToss() (Вернет 0 или 1) Получения рандомного числа 0 или 1
+ coinTossBool() - Пример AScripts.coinTossBool() (Вернет true или false) Получения рандомно true или false
 
-Облегченые версии скрипта $gameSelfSwitches.value(key). В них не нужно указывать ИД карты. Только ИД события и булевое значение:
+ Облегченые версии скрипта $gameSelfSwitches.value(key). В них не нужно указывать ИД карты. Только ИД события и булевое значение:
 
-gameOnMapSetSelfSwitches(ИД События, Локальный переключатель ('A-D'), true или false); - Пример AScripts.gameOnMapSetSelfSwitches(1, 'A', true); Аналогично как $gameSelfSwitches.setValue([$gameMap.mapId(), 1, 'A'], true);
+ gameOnMapSetSelfSwitches(ИД События, Локальный переключатель ('A-D'), true или false); - Пример AScripts.gameOnMapSetSelfSwitches(1, 'A', true); Аналогично как $gameSelfSwitches.setValue([$gameMap.mapId(), 1, 'A'], true);
 
-gameOnMapGetSelfSwitches(ИД События, Локальный переключатель ('A-D')); - Пример AScripts.gameOnMapGetSelfSwitches(1, 'A'); (Вернет значение локального переключателя A у события) Аналогично как $gameSelfSwitches.value([$gameMap.mapId(), 1, 'A']);
+ gameOnMapGetSelfSwitches(ИД События, Локальный переключатель ('A-D')); - Пример AScripts.gameOnMapGetSelfSwitches(1, 'A'); (Вернет значение локального переключателя A у события) Аналогично как $gameSelfSwitches.value([$gameMap.mapId(), 1, 'A']);
  
+ gameOnMapSetArraySelfSwitches(ИД События Начало, ИД События Конец, Локальный переключатель ('A-D'), true или false) - Пример AScripts.gameOnMapSetArraySelfSwitches(1, 5, 'A', true); Аналогично:
+	for (var i = 1; i <= 6; i++) {
+		$gameSelfSwitches.setValue([$gameMap.mapId(), i, 'A'], true);
+	}
+
+ gameOnMapGetArraySelfSwitches(ИД События Начало, ИД События Конец, Локальный переключатель ('A-D')) - Пример AScripts.gameOnMapGetArraySelfSwitches(1, 5, 'A', false); (Вернет истину если у всех ивентов от 1 до 5 локальный переключятель A выключен)
+
  */
 
 class AScripts 
@@ -49,8 +56,32 @@ class AScripts
 		$gameSelfSwitches.setValue(key, aBool);
 	}
 
+	static gameOnMapSetArraySelfSwitches(aIDEventStart, aIDEventStop, aSelfSwitch, aBool) {
+		var key;
+		for (var i = aIDEventStart; i <= aIDEventStop; i++){
+			key = [$gameMap.mapId(), i, aSelfSwitch];
+			$gameSelfSwitches.setValue(key, aBool);
+		}
+	}
+
 	static gameOnMapGetSelfSwitches(aIDEvent, aSelfSwitch) {
 		var key = [$gameMap.mapId(), aIDEvent, aSelfSwitch];
 		return $gameSelfSwitches.value(key);
+	}
+
+	static gameOnMapGetArraySelfSwitches(aIDEventStart, aIDEventStop, aSelfSwitch, aIsSelfSwitchs) {
+		var key;
+		var a = 0, b = 0;
+		for (var i = aIDEventStart; i <= aIDEventStop; i++){
+			key = [$gameMap.mapId(), i, aSelfSwitch];
+			
+			a++;
+
+			if ($gameSelfSwitches.value(key) === aIsSelfSwitchs){
+				b++;
+			}
+		}
+		
+		return a ===b;
 	}
 }
